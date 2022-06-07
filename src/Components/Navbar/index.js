993edce25625509d1e98d13636399bb6c4581d19
LaelBelchior <laelbelchior@gmail.com>
2022-06-07 15:48:23 -0300

    Criando Navbar

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img
+      className="navbar--img"
+      alt="Logo da Biblioteca"
+      src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" 
+      />
+    </div>
+  );
+}
+
+export default Navbar;
+
