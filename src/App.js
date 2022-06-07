3f504fbdb48b29975ae6fb89b93176739e84bdde
LaelBelchior <laelbelchior@gmail.com>
2022-06-07 18:58:14 -0300

    Refatorando para buscar da API direto da Searchbar e refazendo o uso do useState

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Searchbar from './Components/Searchbar';
-import {searchPokemon} from './api';
-
-const onSearchHandler = async (pokemon) => {
-  const result = await searchPokemon(pokemon)
-  console.log(result)
-}
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Searchbar onSearch={onSearchHandler}/>
+      <Searchbar />
       <div className='App'></div>
     </div>
   );
--- src/Components/Searchbar/index.js
+++ src/Components/Searchbar/index.js
@@ -1,23 +1,29 @@
 import React, {useState} from 'react';
+import {searchPokemon} from '../../api';
 import './style.css';
 
-function Searchbar(props) {
+function Searchbar() {
 
-    const {onSearch} = props
-    const [pokemon, setPokemon] = useState('');
+    const [search, setSearch] = useState('')
+    const [pokemon, setPokemon] = useState('')
 
-    function handlerPokemon(e){
-        setPokemon(e.target.value)
+    function handlerSearch(e){
+        setSearch(e.target.value)
     }
 
     function handlerButton(){
-        onSearch(pokemon)
+        onSearchHandler(search)
+    }
+
+    const onSearchHandler = async (pokemon) => {
+        const result = await searchPokemon(pokemon)
+        setPokemon(result)
     }
 
     return (
         <div className='searchbar--container'>
             <div className='searchbar'>
-                <input name='input-search' placeholder='Busque por um Pokemon' onChange={handlerPokemon}/>
+                <input name='input-search' placeholder='Busque por um Pokemon' onChange={handlerSearch}/>
             </div>
             <div className='searchbar--button'>
                 <button onClick={handlerButton}>Procurar</button>
