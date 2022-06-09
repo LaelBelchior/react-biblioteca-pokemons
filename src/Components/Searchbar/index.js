5a66acdf5ab8884dfcb2c1bdb14b3ab0039126d5
LaelBelchior <laelbelchior@gmail.com>
2022-06-08 21:18:48 -0300

    Arrumando CSS do Searchbar

--- src/Components/Searchbar/index.js
+++ src/Components/Searchbar/index.js
@@ -1,62 +1,69 @@
-import React, {useState} from 'react';
-import {searchPokemon, searchPowerPokemon} from '../../api';
-import Card from '../Card';
-import './style.css';
+import React, { useState } from "react";
+import { searchPokemon, searchPowerPokemon } from "../../api";
+import Card from "../Card";
+import "./style.css";
 
 function Searchbar() {
+  const [search, setSearch] = useState("");
+  const [pokemon, setPokemon] = useState("");
+  const [power, setPower] = useState("");
+  const [damage, setDamage] = useState("");
+  const [effect, setEffect] = useState("");
 
-    const [search, setSearch] = useState('')
-    const [pokemon, setPokemon] = useState('')
-    const [power, setPower] = useState('')
-    const [damage, setDamage] = useState('')
-    const [effect, setEffect] = useState('')
+  function handlerSearch(e) {
+    setSearch(e.target.value);
+  }
 
-    function handlerSearch(e){
-        setSearch(e.target.value)
-    }
+  const handlerButton = async () => {
+    onSearchHandler(search);
+  };
 
-    const handlerButton = async () => {
-        onSearchHandler(search)
-    }
+  const onSearchHandler = async (pokemon) => {
+    const result = await searchPokemon(pokemon);
+    capturePower(result.id);
+    setPokemon(result);
+  };
 
-    const onSearchHandler = async (pokemon) => {
-        const result = await searchPokemon(pokemon)
-        capturePower(result.id)
-        setPokemon(result)
-    }
+  const capturePower = async (pokemon) => {
+    const result = await searchPowerPokemon(pokemon);
+    const power = result.effect_entries[0].short_effect;
+    const damage = result.damage_class.name;
+    const effect = result.effect_entries[0].effect;
+    setDamage(damage);
+    setEffect(effect);
+    setPower(power);
+  };
 
-    const capturePower = async (pokemon) => {
-        const result = await searchPowerPokemon(pokemon)
-        const power = result.effect_entries[0].short_effect
-        const damage = result.damage_class.name
-        const effect = result.effect_entries[0].effect
-        setDamage(damage)
-        setEffect(effect)
-        setPower(power)
-    }
-
-
-
-    return (
-        <div className='searchbar--container'>
-            <div className='searchbar'>
-                <input name='input-search' placeholder='Busque por um Pokemon' onChange={handlerSearch}/>
-            </div>
-            <div className='searchbar--button'>
-                <button type='button' onClick={handlerButton}>Procurar</button>
-            </div>
-            {pokemon ? (
-                <Card 
-                img={pokemon.sprites.front_default}
-                name={pokemon.name}
-                weight={pokemon.weight}
-                power={power}
-                damage={damage}
-                effect={effect}
-                />
-            ) : null}
+  return (
+    <div className="searchbar--container">
+      <div className="searchbar--inputs">
+        <div className="searchbar">
+          <input
+            name="input-search"
+            placeholder="Busque por um Pokemon"
+            onChange={handlerSearch}
+          />
         </div>
-    )
+        <div className="searchbar--button">
+          <button type="button" onClick={handlerButton}>
+            Procurar
+          </button>
+        </div>
+      </div>
+      <div className="searchbar-card">
+        {pokemon ? (
+          <Card
+            img={pokemon.sprites.front_default}
+            name={pokemon.name}
+            weight={pokemon.weight}
+            power={power}
+            damage={damage}
+            effect={effect}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 export default Searchbar;
