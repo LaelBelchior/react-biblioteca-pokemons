7dfedbc6794aec9181bb2eddb4278373badcc9b3
LaelBelchior <laelbelchior@gmail.com>
2022-06-11 07:48:52 -0300

    Realização estrutura cognito

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 import Home from './Components/Home'
 
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Authenticator>
+      {({signOut, user}) => (
+        <div className='authenticator-container'>
+          <Home />
+          <button className='authenticator-button' onClick={signOut}>Sair</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import Store from './Store';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
